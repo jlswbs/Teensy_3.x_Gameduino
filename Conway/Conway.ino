7f991eb0321b4f1b3d9072a1989a23bf9a6ce93e
jlswbs <jlswbs@users.noreply.github.com>
2019-11-10 18:33:18 +0100

    Add files via upload

---
+++ Conway/Conway.ino
@@ -0,0 +1,167 @@
+// Conways Game of Life cellular automata //
+
+#include "SPI.h"
+#include "GD.h"
+#include "randreg.h"
+
+#define WIDTH 256/2
+#define HEIGHT 256/2
+#define MAX_GEN 995
+
+void RandGrid();
+void RunGrid();
+void DrawGrid();
+int Neighbours(int x, int y);
+
+  bool grid[2][WIDTH][HEIGHT];
+  int current, generations;
+  char msg1[30], msg2[30];
+  int i, j;
+   
+byte replicate(byte color)
+{
+  return (color << 6) | (color << 4) | (color << 2) | color;
+}
+           
+void setpixel(byte x, byte y, byte color)
+{
+  unsigned int addr = RAM_SPRIMG|(x & 0xf)|(y << 4)|((x & 0x30)<<8);
+  byte mask = 0xc0 >> ((x >> 5) & 6);
+  GD.wr(addr, (GD.rd(addr) & ~mask) | (color & mask));
+}
+
+
+void setup()
+{
+  int i;
+  GD.begin();
+  GD.microcode(randreg_code, sizeof(randreg_code));
+  GD.ascii();
+  for (i = 0; i < 256; i++) {
+  int x = 72 + 16 * ((i >> 4) & 15);
+  int y = 22 + 16 * (i & 15); 
+  int image = i & 63;
+  int pal =   3-(i >> 6);
+  GD.sprite(i, x, y, image, 0x8 | (pal << 1), 0,0);}
+  GD.fill(RAM_SPRIMG, 0, 16384);
+  GD.wr16(PALETTE4A, RGB(0,0,0));
+  GD.wr16(PALETTE4A + 2, RGB(255,255,0));
+  GD.wr16(PALETTE4A + 4, RGB(0,255,255));
+  GD.wr16(PALETTE4A + 6, RGB(255,255,255));
+  GD.putstr(6, 1, "Conways Game of Life cellular automata");
+  GD.putstr(16, 36, "Created by JLS 2019");
+    
+  RandGrid();
+  DrawGrid();
+}
+
+
+void loop()
+{ 
+  
+  RunGrid();
+  DrawGrid();
+
+  generations++;
+
+  GD.putstr(1, 36, msg1);
+  sprintf(msg1, "%3d cnt", GD.rd(0x2802));
+
+  GD.putstr(41, 36, msg2);
+  sprintf(msg2, "%3d gens", generations);
+  
+  if (generations > MAX_GEN) {
+    generations = 0;
+    RandGrid();
+  }
+
+  GD.waitvblank();
+    
+}
+
+void RandGrid()
+{
+  GD.fill(RAM_SPRIMG, 0, 16384);
+  current = 0;
+  generations = 0;
+  for (i = 0; i < WIDTH; i++) {
+    for (j = 0; j < HEIGHT; j++) {
+      grid[0][i][j] =  GD.rd(0x280E)%2;
+      grid[1][i][j] = 0;
+    }
+  }
+}
+
+void RunGrid()
+{
+  int x, y;
+  int count;
+  int value = 0;
+  int new_grid;
+
+  new_grid = 1 - current;
+  
+  for (y = 0; y < HEIGHT; y++) {
+  
+    for (x = 0; x < WIDTH; x++) {
+      
+      count = Neighbours(x, y);
+      
+      if (count < 2 || count > 3) { value = 0; }
+      else if (count == 3) { value = 3; }
+      else { value = grid[current][x][y]; }
+    
+      grid[new_grid][x][y] = value;
+    
+    }
+  }
+  
+  current = new_grid;
+}
+
+
+int Neighbours(int x, int y)
+{
+  int i, j;
+  int result = 0;
+
+  x--;
+  y--;
+  
+  for (i = 0; i < 3; i++) {
+  
+    if (y < 0 || y > (HEIGHT - 1)) continue;
+
+    for (j = 0; j < 3; j++) {
+      if (x < 0 || x > (WIDTH - 1)) continue;
+      if (i==1 && j == 1) { x++; continue; }
+      if (grid[current][x][y]) result++;
+      x++;
+    }
+    y++;
+    x -= 3;
+  }
+  
+  return result;
+} 
+
+
+void DrawGrid()
+{
+  int  x,  y;
+  int cx, cy;
+  cx = 0;
+  cy = 0;
+  
+  for (y = 0; y < HEIGHT; y++) {
+    cx = 0;
+    for (x = 0; x < WIDTH; x++) {
+      if (grid[1-current][x][y] != grid[current][x][y]) {
+        if(grid[current][x][y]) setpixel(2*cx, 2*cy, replicate(1));
+        else setpixel(2*cx, 2*cy, replicate(0));
+        }
+    cx += 1;
+    }
+    cy += 1;
+  }
+}
---
+++ StarWars/StarWars.ino
@@ -0,0 +1,182 @@
+// StarWars cellular automata //
+
+#include "SPI.h"
+#include "GD.h"
+#include "randreg.h"
+
+#define WIDTH 244/2
+#define HEIGHT 242/2
+#define MAX_GEN 750
+
+void step();
+int wrap(int v, int m);
+void draw_type(int min_alive, int max_alive, int r);
+void randseed();
+
+  uint8_t current [WIDTH][HEIGHT];
+  uint8_t next [WIDTH][HEIGHT];
+  uint8_t alive_counts [WIDTH][HEIGHT];
+  uint8_t tmp[WIDTH][HEIGHT];
+  
+  int ALIVE = 3;
+  int DEATH_1 = 2;
+  int DEAD = 0;
+
+  int x,y,nx,ny;
+  int generations;
+  char msg1[30], msg2[30];
+  
+   
+byte replicate(byte color)
+{
+  return (color << 6) | (color << 4) | (color << 2) | color;
+}
+           
+void setpixel(byte x, byte y, byte color)
+{
+  unsigned int addr = RAM_SPRIMG|(x & 0xf)|(y << 4)|((x & 0x30)<<8);
+  byte mask = 0xc0 >> ((x >> 5) & 6);
+  GD.wr(addr, (GD.rd(addr) & ~mask) | (color & mask));
+}
+
+
+void setup()
+{
+  int i;
+  GD.begin();
+  GD.microcode(randreg_code, sizeof(randreg_code));
+  GD.ascii();
+  for (i = 0; i < 256; i++) {
+  int x = 72 + 16 * ((i >> 4) & 15);
+  int y = 22 + 16 * (i & 15); 
+  int image = i & 63;
+  int pal =   3-(i >> 6);
+  GD.sprite(i, x, y, image, 0x8 | (pal << 1), 0,0);}
+  GD.fill(RAM_SPRIMG, 0, 16384);
+  GD.wr16(PALETTE4A, RGB(0,0,0));
+  GD.wr16(PALETTE4A + 2, RGB(255,255,0));
+  GD.wr16(PALETTE4A + 4, RGB(0,255,255));
+  GD.wr16(PALETTE4A + 6, RGB(255,0,255));
+  GD.putstr(12, 1, "StarWars cellular automata");
+  GD.putstr(16, 36, "Created by JLS 2019");
+    
+  randseed();
+}
+
+
+void loop()
+{ 
+  step();
+      
+  GD.fill(RAM_SPRIMG, 0, 16384);
+      
+  draw_type(50,100, 3);
+  draw_type(2,49, 2);
+  draw_type(0,1, 1);
+
+  generations++;
+
+  GD.putstr(1, 36, msg1);
+  sprintf(msg1, "%3d cnt", GD.rd(0x2802));
+
+  GD.putstr(41, 36, msg2);
+  sprintf(msg2, "%3d gens", generations);
+  
+  if (generations > MAX_GEN) {
+    generations = 0;
+    randseed();
+  }
+
+  GD.waitvblank();
+}
+
+void randseed() {
+  
+  x = 0;
+  y = 0;
+
+  for (x = 0; x < WIDTH; x++) {
+    for (y = 0; y < HEIGHT; y++) {
+    
+      current[x][y] = GD.rd(0x280E)%4;
+      alive_counts[x][y] = 0;  
+    }
+  }
+
+  GD.fill(RAM_SPRIMG, 0, 16384);
+}
+
+
+void step() {
+ 
+  for (x = 0; x < WIDTH; x++) {
+  
+    for (y = 0; y < HEIGHT; y++) {
+    
+      int count = 0;
+      int next_val;
+      int mx = WIDTH-1;
+      int my = HEIGHT-1;
+      int self = current[x][y];
+    
+    for (nx = x-1; nx <= x+1; nx++) {
+  
+      for (ny = y-1; ny <= y+1; ny++) {
+    
+        if (nx == x && ny == y) continue;
+      
+        if (current[wrap(nx, mx)][wrap(ny, my)] == ALIVE) count++;
+      
+      }   
+    }  
+
+    int neighbors = count;
+    
+    if (self == ALIVE) next_val = ((neighbors == 3) || (neighbors == 4) || (neighbors == 5)) ? ALIVE : DEATH_1;
+  
+    else if (self == DEAD) next_val = (neighbors == 2) ? ALIVE : DEAD;
+  
+    else next_val = self-1;
+   
+    next[x][y] = next_val;
+  
+    if (next_val == ALIVE) {
+    
+      if ((alive_counts[x][y]+1) <= 100) alive_counts[x][y] = (int)(alive_counts[x][y]+1);
+        
+      } else if (next_val == DEAD) alive_counts[x][y] = 0;
+      }
+    }
+    
+    for (x = 0; x < WIDTH; x++) {
+      for (y = 0; y < HEIGHT; y++) {
+        tmp[x][y] = current[x][y];
+        current[x][y] = next[x][y];
+        next[x][y] = tmp[x][y];
+      }
+    }
+}
+  
+
+  
+int wrap(int v, int m) {
+
+  if (v < 0) return v + m;
+  else if (v >= m) return v - m;
+  else return v;
+}
+  
+  
+void draw_type(int min_alive, int max_alive, int r) {  
+    
+  for (x = 0; x < WIDTH; x++) { 
+    for (y = 0; y < HEIGHT; y++) {
+      int self = current[x][y];  
+      if (self == DEAD) continue;  
+      int alive = alive_counts[x][y];
+      if (alive < min_alive || alive > max_alive) continue;
+      setpixel (6+(2*x), 7+(2*y), replicate(r));
+  }
+}
+ 
+  }
